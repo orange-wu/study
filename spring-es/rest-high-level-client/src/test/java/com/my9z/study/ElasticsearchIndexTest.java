2565014906872e9e8f9c93b1ead165424fd7455e
wczy <wuchengzhi369@gmail.com>
2022-11-03 21:52:16 +0800

    feat:es 索引相关操作API

--- spring-es/rest-high-level-client/src/test/java/com/my9z/study/ElasticsearchIndexTest.java
+++ spring-es/rest-high-level-client/src/test/java/com/my9z/study/ElasticsearchIndexTest.java
@@ -1,7 +1,17 @@
 package com.my9z.study;
 
+import cn.hutool.json.JSONUtil;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.client.indices.GetIndexResponse;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,5 +29,31 @@ public class ElasticsearchIndexTest {
 
     @Autowired
     private RestHighLevelClient restHighLevelClient;
+    @SneakyThrows
+    @Test
+    public void createIndexTest(){
+        CreateIndexRequest wczy = new CreateIndexRequest("wczy");
+        CreateIndexResponse createResponse = restHighLevelClient.indices().create(wczy, RequestOptions.DEFAULT);
+        log.info("result:{}",createResponse.isAcknowledged());
+        log.info("response:{}",createResponse);
+    }
+
+    @SneakyThrows
+    @Test
+    public void getIndexTest(){
+        GetIndexRequest wczy = new GetIndexRequest("wczy");
+        GetIndexResponse getIndexResponse = restHighLevelClient.indices().get(wczy, RequestOptions.DEFAULT);
+        log.info("aliases:{}",getIndexResponse.getAliases());
+        log.info("mappings:{}",getIndexResponse.getMappings());
+        log.info("settings:{}",getIndexResponse.getSettings());
+        log.info("getIndexResponse:{}", JSONUtil.toJsonStr(getIndexResponse));
+    }
+    @SneakyThrows
+    @Test
+    public void deleteIndexTest(){
+        DeleteIndexRequest wczy = new DeleteIndexRequest("wczy");
+        AcknowledgedResponse deleteResponse = restHighLevelClient.indices().delete(wczy, RequestOptions.DEFAULT);
+        log.info("deleteResponse:{}",JSONUtil.toJsonStr(deleteResponse));
+    }
 
 }
