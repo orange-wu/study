4f4d935182d2326859c3782c7e690f22907206c0
wczy <wuchengzhi369@gmail.com>
2022-11-05 09:39:11 +0800

    feat:es 创建index指定主分片和副本分片数量，指定mapping的api

--- spring-es/rest-high-level-client/src/test/java/com/my9z/study/ElasticsearchIndexTest.java
+++ spring-es/rest-high-level-client/src/test/java/com/my9z/study/ElasticsearchIndexTest.java
@@ -11,6 +11,7 @@ import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.client.indices.GetIndexResponse;
+import org.elasticsearch.common.settings.Settings;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,31 +30,37 @@ public class ElasticsearchIndexTest {
 
     @Autowired
     private RestHighLevelClient restHighLevelClient;
+
     @SneakyThrows
     @Test
-    public void createIndexTest(){
+    public void createIndexTest() {
         CreateIndexRequest wczy = new CreateIndexRequest("wczy");
+        //指定索引主分片和副本分片的数量
+        wczy.settings(Settings.builder().put("index.number_of_shards", 3).put("index.number_of_replicas", 1));
+        //指定索引的mapping格式
+        //wczy.mapping("...", XContentType.JSON);
         CreateIndexResponse createResponse = restHighLevelClient.indices().create(wczy, RequestOptions.DEFAULT);
-        log.info("result:{}",createResponse.isAcknowledged());
-        log.info("response:{}",createResponse);
+        log.info("result:{}", createResponse.isAcknowledged());
+        log.info("response:{}", createResponse);
     }
 
     @SneakyThrows
     @Test
-    public void getIndexTest(){
+    public void getIndexTest() {
         GetIndexRequest wczy = new GetIndexRequest("wczy");
         GetIndexResponse getIndexResponse = restHighLevelClient.indices().get(wczy, RequestOptions.DEFAULT);
-        log.info("aliases:{}",getIndexResponse.getAliases());
-        log.info("mappings:{}",getIndexResponse.getMappings());
-        log.info("settings:{}",getIndexResponse.getSettings());
+        log.info("aliases:{}", getIndexResponse.getAliases());
+        log.info("mappings:{}", getIndexResponse.getMappings());
+        log.info("settings:{}", getIndexResponse.getSettings());
         log.info("getIndexResponse:{}", JSONUtil.toJsonStr(getIndexResponse));
     }
+
     @SneakyThrows
     @Test
-    public void deleteIndexTest(){
+    public void deleteIndexTest() {
         DeleteIndexRequest wczy = new DeleteIndexRequest("wczy");
         AcknowledgedResponse deleteResponse = restHighLevelClient.indices().delete(wczy, RequestOptions.DEFAULT);
-        log.info("deleteResponse:{}",JSONUtil.toJsonStr(deleteResponse));
+        log.info("deleteResponse:{}", JSONUtil.toJsonStr(deleteResponse));
     }
 
 }
