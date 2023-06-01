338761c0a3c308a5be54efed3b4c14f7187ee3d9
wczy <wuchengzhi369@gmail.com>
2023-06-01 11:17:30 +0800

    feat:《Java并发编程之美》-wait方法只会释放当前共享资源上的锁（注释修正）

--- multi-thread/src/main/java/wait_and_notify/WaitReleaseOneLock.java
+++ multi-thread/src/main/java/wait_and_notify/WaitReleaseOneLock.java
@@ -2,6 +2,10 @@ package wait_and_notify;
 
 /**
  * @description: 当前线程调用共享变量的wait()方法只会释放当前共享变量上的锁，如果当前线程还持有其他共享变量的锁，则这些锁不会释放。
+ * 以下代码线程A先获取到resourceA和resourceB共享变量的锁，然后调用了resourceA的wait方法阻塞自己，释放掉resourceA上的锁，
+ * 线程B休眠结束后首先尝试获取resourceA上的锁，如果当时线程A还没有调用wait方法释放该锁，那么线程B就会被阻塞，当线程A释放了resourceA上的锁后，
+ * 线程B就会获取到resourceA上的锁，然后尝试获取resourceB上的锁，由于线程A调用的是resourceA的wait方法，所以线程A挂起自己后并没有释放resourceB上的锁，
+ * 所以线程B尝试获取resourceB上的锁时会被阻塞。
  * @author: wczy9
  * @createTime: 2023-03-15  09:41
  */
